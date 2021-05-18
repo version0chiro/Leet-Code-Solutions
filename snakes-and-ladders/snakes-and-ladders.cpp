ed07679287c16c43e2e7c19e401bfd906d47f88a
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-05-18 16:15:45 +0530

    Time: 16 ms (76.22%), Space: 12.4 MB (84.54%) - LeetHub

--- snakes-and-ladders/snakes-and-ladders.cpp
+++ snakes-and-ladders/snakes-and-ladders.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
-    int get(int s,int n){
-        int quot=(s-1)/n;
-        int rem=(s-1)%n;
-        int row=n-quot-1;
+    int get(int x,int n){
+        int que = (x-1)/n;
+        int rem = (x-1)%n;
+        int row = n- que -1 ;
         int col=row%2!=(n%2) ? rem : n-1-rem;
-        return row*n+col;
+        return (row*n) + col;
     }
     int snakesAndLadders(vector<vector<int>>& board) {
-        int n = board.size();
-        vector<bool> vis(n*n+1,false);
         queue<int> q;
+        int n = board.size();
         q.push(1);
-        vis[1]=true;
         int ans=-1;
+        vector<bool> vis(n*n+1,false);
+        vis[1]=true;
         while(!q.empty()){
             ans++;
-            int n1=q.size();
+            int n1 = q.size();
             while(n1--){
                 int s = q.front();
                 q.pop();
-
-                if(s==(n*n)){
-                    return ans;
-                }
+                if(s==(n*n)) return ans;
+                
                 for(int i=1;i<=6;i++){
                     if((s+i)<=n*n){
                         int ns = get(s+i,n);
                         int x = ns/n;
                         int y = ns%n;
-                        int fis=board[x][y]==-1?s+i:board[x][y];
+                        int fis = board[x][y]==-1?s+i:board[x][y];
                         if(!vis[fis]){
-                            q.push(fis);
                             vis[fis]=true;
+                            q.push(fis);
                         }
+   
                     }
                 }
             }
         }
-        
         return -1;
     }
 };
