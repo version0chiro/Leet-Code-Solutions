cade1e1bf02f236ec328ea1e0240c809bb12607a
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-05-24 19:01:34 +0530

    Time: 52 ms (89.86%), Space: 10.5 MB (83.19%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        int dp[++amount];
-        
-        dp[0]=0;
-        
-        sort(coins.begin(),coins.end());
-        
-        for(int i=1;i<amount;i++){
-            dp[i]=INT_MAX;
-            
-            for(auto c:coins){
-                if(i-c<0) break;
-                
-                if(dp[i-c]!=INT_MAX) dp[i]=min(dp[i],1+dp[i-c]);
+    
+    int minCoin(vector<int>& coin,int S,int dp[])
+    {
+        if(S==0)
+            return 0;
+        if(S<0)
+            return INT_MAX;
+        if(dp[S]!=-1)
+            return dp[S];
+        int mini=INT_MAX;
+        for(int i=0;i<coin.size();i++)
+        {
+            if(coin[i]<=S)
+            {
+                int res=minCoin(coin,S-coin[i],dp);
+                if(res!=INT_MAX and res<mini)
+                    mini=res+1;
             }
         }
         
-        return dp[--amount]==INT_MAX?-1:dp[amount];
+        return dp[S]=mini;
+            
+    }
+    
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        int dp[amount+5];
+        memset(dp,-1,sizeof dp);
+        int res= minCoin(coins,amount,dp);
+        return (res==INT_MAX)?-1:res;
     }
 };
