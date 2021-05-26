bcb689ae05e1f85bec7e82784bbb313e5910068b
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-05-26 11:58:38 +0530

    Time: 108 ms (64.67%), Space: 17.8 MB (39.42%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
-    bool ans = false;
-    int dp[201][20001];
-    
-    bool help(int pos,int sum,vector<int> nums){
-        if(sum==0)
-            return true;
-        if(pos>=nums.size() or sum<0){
-            return false;
-        }
-        if(dp[pos][sum] != -1)
-            return dp[pos][sum];
-        
-        
-        int t=nums[pos];
-        
-        return dp[pos][sum]= help(pos+1,sum-t,nums) or help(pos+1,sum,nums);
-        
-    }
     bool canPartition(vector<int>& nums) {
-        int sum =0;
+        int sum=0;
+        
         for(auto a:nums){
             sum+=a;
         }
-        if(sum%2==1){
-            return false;
-        }
-        memset(dp, -1, sizeof(dp));
         
-        return help(0,sum/2,nums);
+        if(sum%2!=0)
+            return false;
+        
+        int halfSum=sum/2;
+        int t[nums.size()+1][halfSum+1];
+        
+        
+        for(int j=0;j<halfSum+1;j++){
+            t[0][j] = false;
+        }
+        
+        for(int i=0;i<nums.size()+1;i++){
+            t[i][0] = true;
+        }
+        
+        
+        for(int i=1;i<=nums.size();i++){
+            for(int j=1;j<=halfSum;j++){
+                if(nums[i-1]<=j){
+                    t[i][j] = t[i-1][j-nums[i-1]]||t[i-1][j];
+                }else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        
+        return t[nums.size()][halfSum];
+    
     }
 };
