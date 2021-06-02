1f25f4ec429b26d0c0bbc8233cf363f21aa55af3
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-06-02 16:42:43 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (20.52%) - LeetHub

--- is-subsequence/is-subsequence.cpp
+++ is-subsequence/is-subsequence.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-       queue<char> st;
-       for(auto a:s){
-           st.push(a);
-       }
+    bool isSubsequence(string s, string s2) {
+        int n=s.size();
+        int m = s2.size();
         
-       for(auto a:t){
-           if(st.empty()){return true;}
-           if(st.front()==a){
-               st.pop();
-           }
-       }
-       
-      return st.empty(); 
-       
+        int t[n+1][m+1];
+        
+        memset(t,0,sizeof(t));
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s[i-1]==s2[j-1]){
+                    t[i][j]=1+t[i-1][j-1];
+                }else{
+                    t[i][j]=max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        
+        return n==t[n][m];
     }
 };
