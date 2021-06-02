5496c31c8c27b88c3c67a5feb6b350073ca9e190
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-06-02 15:17:47 +0530

    Time: 36 ms (44.15%), Space: 7.6 MB (50.82%) - LeetHub

--- minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minInsertions(string s) {
         string s1=s;
-        reverse(s1.begin(),s1.end());
+        // reverse(s1.begin(),s1.end());
         
         int n = s.length();
         
@@ -12,7 +12,7 @@ public:
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                if(s[i-1]==s1[j-1]){
+                if(s[i-1]==s[n-j]){
                     t[i][j]=1+t[i-1][j-1];
                 }else{
                     t[i][j]=max(t[i-1][j],t[i][j-1]);
