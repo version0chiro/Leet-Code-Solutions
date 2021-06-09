869d6ef2d35ec8e0c23fb82d32ebf3b325f1e2b8
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-06-09 15:38:16 +0530

    Time: 96 ms (23.01%), Space: 48.3 MB (28.30%) - LeetHub

--- jump-game/jump-game.cpp
+++ jump-game/jump-game.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        if(nums.size()==1){
+    bool canJump(vector<int>& arr) {
+        if(arr.size()==1){
             return true;
         }
-        int maxDistance = 0;
-        for(int i=0 ; i<nums.size() && i<=maxDistance ;i++){
-            maxDistance = max(maxDistance,nums[i]+i);
-            
-        }
+        int n = arr.size();
         
-        if(nums.size()-1<=maxDistance){
-            return true;
+        int remaingJumps=arr[0];
+        int jumpsTaken=1;
+        int maxJumpSoFar=INT_MIN;
+        
+        int iterationIndex=0;
+        int diffFromMax=0;
+        if(remaingJumps<1){
+            return false;
         }
-        return false;
+        while(iterationIndex<n-1){
+            // cout<<arr[iterationIndex]<<" ";
+            if(remaingJumps>0){
+                iterationIndex++;
+                remaingJumps--;
+            }else if(remaingJumps==0){
+                // cout<<"jumps over: new jump "<<maxJumpSoFar<<"\n";
+                jumpsTaken++;
+                if(iterationIndex>=maxJumpSoFar){
+                    return false;
+                }
+                remaingJumps=maxJumpSoFar-iterationIndex;
+            }
+            
+            maxJumpSoFar=max(maxJumpSoFar,arr[iterationIndex]+iterationIndex);
+        }
+        // cout<<"\n";
+        return true;
     }
 };
