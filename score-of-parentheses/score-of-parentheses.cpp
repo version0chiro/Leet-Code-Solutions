d59fb374f85b52912bf85b7aa9517b5ff376ae00
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-06-10 15:41:27 +0530

    Time: 0 ms (100.00%), Space: 6 MB (91.98%) - LeetHub

---
+++ score-of-parentheses/score-of-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        
+        int sum=0;
+        int openCount=0;
+        int finalSum=0;
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                openCount++;
+            }
+            else if(s[i]==')'){
+                openCount--;
+                int powerRaise=0;
+                while(i+1<s.size() && s[i+1]==')'){
+                    i++;
+                    openCount--;
+                    powerRaise++;
+                }
+                cout<<openCount<<" ";
+                cout<<pow(2,openCount)*pow(2,powerRaise)<<"\n";
+                sum+=pow(2,openCount)*pow(2,powerRaise);
+            }
+        }
+        
+        return sum;
+    }
+};
