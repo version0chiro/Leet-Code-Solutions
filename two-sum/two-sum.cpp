a466707bbbb7d8728e020841fdbcca70ff55fc3d
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-06-18 11:56:44 +0530

    Time: 12 ms (56.49%), Space: 10.7 MB (35.04%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -4,14 +4,15 @@ public:
         unordered_map<int,int> m;
         
         for(int i=0;i<nums.size();i++){
+             if(m.find(target-nums[i])!=m.end() && m[target-nums[i]]!=i){
+                return {m[target-nums[i]],i};
+            }
             m[nums[i]]=i;
         }
         
-        for(int i=0;i<nums.size();i++){
-            if(m.find(target-nums[i])!=m.end() && m[target-nums[i]]!=i){
-                return {m[target-nums[i]],i};
-            }
-        }
+//         for(int i=0;i<nums.size();i++){
+           
+//         }
         
         
         return {-1,-1};
