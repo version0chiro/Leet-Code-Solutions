e056cfc9805e98d43f9f44d8ecb57a773fd85125
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-06-19 12:42:07 +0530

    Time: 16 ms (40.16%), Space: 9.9 MB (9.27%) - LeetHub

--- 3sum-closest/3sum-closest.cpp
+++ 3sum-closest/3sum-closest.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
         int minDist =INT_MAX;
         int ans=0;
         for(int i=0;i<nums.size()-2;i++){
             int left=i+1;
+            int right = nums.size()-1;
             
-            for(int j=left;j<nums.size()-1;j++){
-                
-                for(int k=j+1;k<nums.size();k++){
-                    int sum = nums[i]+nums[j]+nums[k];
-                    
-                    if(abs(target-sum)<minDist){
+            while(left<right){
+                int sum = nums[i]+nums[left]+nums[right];
+                if(abs(target-sum)<minDist){
                         minDist=abs(target-sum);
                         ans=sum;
-                    }
-                    // cout<<sum<<"\n";
+                }
+                
+                if(sum>target){
+                    right--;
+                }else{
+                    left++;
                 }
             }
-            // cout<<nums[i]<<" "<<nums[left]<<" "<<nums[right]<<"\n";
-            
         }
         
         return ans;
