d742898e024491539855af8f918d077b214abb44
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-06-25 17:41:02 +0530

    Time: 4 ms (80.12%), Space: 14.2 MB (29.29%) - LeetHub

---
+++ vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sCol=INT_MAX,eCol=INT_MIN,sRow=INT_MAX,eRow=INT_MIN;
+    map<pair<int,int>,vector<int>> m;
+    void solve(TreeNode* root,int col,int row){
+        if(!root) return;
+        
+        sCol=min(col,sCol),eCol=max(col,eCol);
+        sRow=min(row,sRow),eRow=max(row,eRow);
+        m[{row,col}].push_back(root->val);
+        solve(root->left,col-1,row+1);
+        solve(root->right,col+1,row+1);
+    }
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        solve(root,0,0);
+        vector<vector<int>> ans;
+        vector<int> temp;
+        
+        for(int i=sCol;i<=eCol;i++){
+            for(int j=sRow;j<=eRow;j++){
+                if(m.find({j,i})!=m.end()){
+                    if(m[{j,i}].size()>1)
+                        sort(m[{j,i}].begin(),m[{j,i}].end());
+        
+                    // ans.push_back(m[{j,i}]);
+                    // cout<<" "<<j<<" , "<<i<<" :";
+                    for(auto a:m[{j,i}]){
+                        temp.push_back(a);
+                    }
+                }
+            }
+            ans.push_back(temp);
+            temp.clear();
+            // cout<<endl;
+        }
+        // for(auto a:m){
+        //     // if(a.second.size()>1)
+        //     //     sort(a.second.begin(),a.second.end());
+        //     // ans.push_back(a.second);
+        //     cout<<a.first.first<<","<<a.first.second<<"::";
+        //     for(auto p:a.second){
+        //         cout<<p<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return ans;
+    }
+};
