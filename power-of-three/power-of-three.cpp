23155152bafcb223aff7704d5f4b934e9e22a073
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-06-28 11:47:43 +0530

    Time: 24 ms (21.53%), Space: 5.9 MB (30.64%) - LeetHub

--- power-of-three/power-of-three.cpp
+++ power-of-three/power-of-three.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==0 ) return false;
-        while(n>1){
-            cout<<n<<" ";
-            if(n%3!=0) return false;
-            n=n/3;
-        }
-        
-        return n==1;
+            return ( n>0 &&  1162261467%n==0);
+
     }
 };
