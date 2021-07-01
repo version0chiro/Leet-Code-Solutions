e59b64b01e7eaa54b26a323cf18398a12e86b381
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-07-01 12:00:37 +0530

    Time: 28 ms (91.82%), Space: 26.5 MB (86.51%) - LeetHub

--- serialize-and-deserialize-bst/serialize-and-deserialize-bst.cpp
+++ serialize-and-deserialize-bst/serialize-and-deserialize-bst.cpp
@@ -10,54 +10,42 @@
 class Codec {
 public:
 
-    // Encodes a tree to a single string.
-    void inOrder(TreeNode* root,string &ans){
-        if(!root) {
-            ans+="N|";
-            return;
+    inline void inorderDFS(TreeNode* root,string &order){
+        if(!root) return;
+        char buff[4];
+        
+        memcpy(buff,&(root->val),sizeof(int));
+        
+        for(int i=0;i<4;i++){
+            order.push_back(buff[i]);
         }
-        ans+=to_string(root->val)+"|";
-        inOrder(root->left,ans);
-        inOrder(root->right,ans);
+        inorderDFS(root->left,order);
+        inorderDFS(root->right,order);
     }
+    // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if(!root) return "";
-        string serializedTree ="";
-        inOrder(root,serializedTree);
-        return serializedTree;
+        string order;
+        inorderDFS(root,order);
+        return order;
     }
 
-    int i=0;
     // Decodes your encoded data to tree.
-    TreeNode* formTree(TreeNode* root,vector<int> data){
-        // cout<<i<<": ";
-        if(data[i]==-1 || i>=data.size()){
-            i++;
-            return NULL;
-        }else{
-            // cout<<data[i]<<" ";
-            root->val=(data[i]);
-        }
-        i++;
-        root->left=formTree(new TreeNode(),data);
-        root->right=formTree(new TreeNode(),data);
-        
-        return root;
-        
-    }
     TreeNode* deserialize(string data) {
-        if(data=="") return NULL;
-        stringstream ss(data);
-        string item;
-        vector<int> data_vec;
-        while (getline (ss, item, '|')){
-            if(item=="N"){
-                data_vec.push_back(-1);
-            }else
-                data_vec.push_back(stoi(item));
-        }
-        return formTree(new TreeNode(),data_vec);
-        // return NULL;
+        int pos=0;
+        return reconstruct(data,pos,INT_MIN,INT_MAX);
+    }
+    
+    inline TreeNode* reconstruct(const string& buffer,int& pos,int minValue,int maxValue){
+        if(pos>=buffer.size()) return NULL;
+        int value;
+        memcpy(&value,&buffer[pos],sizeof(int));
+        if(value<minValue || value>maxValue) return NULL;
+        TreeNode* root = new TreeNode(value);
+        
+        pos+=sizeof(int);
+        root->left = reconstruct(buffer,pos,minValue,value);
+        root->right = reconstruct(buffer,pos,value,maxValue);
+        return root;
     }
 };
 
