2601487600e730958eac4d387b3b50cc7b2dbc6c
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-07-04 15:40:14 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (78.32%) - LeetHub

--- sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -11,23 +11,23 @@
  */
 class Solution {
 public:
-    vector<int> ans;
+    int sum=0;
     void traverse(TreeNode* root,int last,int d){
         if(!root) return;
         last=last*10+root->val;
-        if(!root->left && !root->right) ans.push_back(last);
+        if(!root->left && !root->right) sum+=last;
         
         traverse(root->left,last,d+1);
         traverse(root->right,last,d+1);
     }
     int sumNumbers(TreeNode* root) {
-        int sum=0;
+        // int sum=0;
         
         traverse(root,0,0);
         
-        for(auto a:ans){
-            sum+=a;
-        }
+//         for(auto a:ans){
+//             sum+=a;
+//         }
         
         return sum;
     }
