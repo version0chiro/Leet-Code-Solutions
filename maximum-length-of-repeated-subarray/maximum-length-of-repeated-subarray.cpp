6c17d28a7b68972d4c581ce35439ff2dc0c9af43
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-07-08 16:56:33 +0530

    Time: 248 ms (56.74%), Space: 16 MB (76.77%) - LeetHub

--- maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
+++ maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
@@ -6,29 +6,31 @@ public:
         int t[n+1][m+1];
         
         for(int i=0;i<n+1;i++){
-            for(int j=0;j<m+1;j++){
-                if(i==0 || j==0)
-                    t[i][j]=0;
-            }
+            t[i][0]=0;
+        }
+        for(int i=0;i<m+1;i++){
+            t[0][i]=0;
         }
         
+        int ans =0;
+
         for(int i=1;i<n+1;i++){
             for(int j=1;j<m+1;j++){
                 if(nums1[i-1]==nums2[j-1]){
                     t[i][j]=1+t[i-1][j-1];
+                    ans=max(t[i][j],ans);
                 }else{
                     t[i][j]=0;
                 }
             }
         }
         
-        int ans =0;
         
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<m+1;j++){
-                ans = max(ans,t[i][j]);
-            }
-        }
+//         for(int i=0;i<n+1;i++){
+//             for(int j=0;j<m+1;j++){
+//                 ans = max(ans,t[i][j]);
+//             }
+//         }
         
         return ans;
     }
