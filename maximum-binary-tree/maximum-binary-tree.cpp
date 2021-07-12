4c47b05a3241e94c8977e9d723fa24a99e1521c8
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-07-12 11:06:45 +0530

    Time: 60 ms (99.39%), Space: 39.4 MB (93.86%) - LeetHub

--- maximum-binary-tree/maximum-binary-tree.cpp
+++ maximum-binary-tree/maximum-binary-tree.cpp
@@ -11,32 +11,24 @@
  */
 class Solution {
 public:
-    int find_maxItem(vector<int>& nums){
-        int maxE=INT_MIN,index=-1;
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        vector<TreeNode*> stk;
         for(int i=0;i<nums.size();i++){
-            if(maxE<nums[i])
-                maxE=nums[i],index=i;
+            TreeNode* curr = new TreeNode(nums[i]);
+            
+            while(!stk.empty() && stk.back()->val<nums[i]){
+                curr->left = stk.back();
+                stk.pop_back();
+            }
+            
+            if(!stk.empty()){
+                stk.back()->right=curr;
+                
+            }
+            
+            stk.push_back(curr);
         }
         
-        return index;
-    }
-    TreeNode* solve(vector<int>& nums,int maxIndex){
-        if(nums.size()<1) return NULL;
-        
-        
-        TreeNode* root = new TreeNode(nums[maxIndex]);
-        
-        vector<int> left (nums.begin(),nums.begin()+maxIndex);
-        vector<int> right (nums.begin()+maxIndex+1,nums.end());
-        
-        root->left=solve(left,find_maxItem(left));
-        root->right=solve(right,find_maxItem(right));
-        
-        return root;
-    }
-    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        
-        return solve(nums,find_maxItem(nums));
-        
+        return stk.front();
     }
 };
