3e0e3fb2b470bacc22d1ae0670b229488b521812
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-07-16 14:19:25 +0530

    Time: 20 ms (6.55%), Space: 11.7 MB (89.06%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.cpp
+++ maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int maxProduct(vector<int>& A) {
-        int n = A.size(),res=A[0],l=0,r=0;
-        
+        int n = A.size(),res=A[0],l=0,r=0,t=-1;
+        // t?cout<<"true":cout<<"false";
         for(int i=0;i<n;i++){
             l=(l?l:1)*A[i];
             r=(r?r:1)*A[n-1-i];
-            // cout<<l<<" "<<r<<"\n";
+            // co
+            cout<<l<<" "<<r<<"\n";
             res=max(res,max(l,r));
         }
         
