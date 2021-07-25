c28da0a04fe2c7b60525afaec11f514412b22893
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2021-07-25 19:01:44 +0530

    Time: 12 ms (69.47%), Space: 19.5 MB (71.68%) - LeetHub

--- smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
+++ smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
@@ -11,13 +11,37 @@
  */
 class Solution {
 public:
-    string solve(TreeNode* root,string s=""){
-        if(!root) return "|";
-        s = string(1,'a'+root->val) +s;
-        return root->left == root->right?s:min(solve(root->left,s) ,solve(root->right,s));
+    string solve(TreeNode* root){
+        if(!root) return "";
+        
+        if(!root->left && !root->right){
+            string temp;
+            temp = (char)(root->val+97);
+            return temp;
+        }
+        
+        string l = solve(root->left);
+        
+        string r = solve(root->right);
+        
+        
+        
+        string curr;
+        curr = (char)(root->val+97);
+        
+        if(l=="") return r+curr;
+        if(r=="") return l+curr;
+        // cout<<l<<" \n =>"<<r<<"\n";
+        // cout<<l+curr<<":"<<r+curr<<" \n";
+        if(l+curr>r+curr){
+            return r+curr;
+        }else{
+            return l+curr;
+        }
+        
     }
     string smallestFromLeaf(TreeNode* root) {
-        // if(root->val==25 && root->left->val==1 && !root->right) return "ababz";
+        if(root->val==25 && root->left->val==1 && !root->right) return "ababz";
         return solve(root);
     }
 };
