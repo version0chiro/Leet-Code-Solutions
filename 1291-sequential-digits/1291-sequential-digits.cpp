dc79069903c53226da5353a249fab972e1540e8d
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-01-23 13:09:18 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (76.43%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -8,16 +8,16 @@ public:
         int temp = 0;
         
         for(int i=1;i<9;i++){
-            cout<<i<<":";
+            // cout<<i<<":";
             temp=i;
             for(int j=i+1;j<=9;j++){
                 temp=temp*10+j;
                 if(temp>=low && temp<=high) seq.push_back(temp);
-                cout<<temp<<"\n";
-                cout<<j<<",";
+                // cout<<temp<<"\n";
+                // cout<<j<<",";
             }
             
-            cout<<"\n";
+            // cout<<"\n";
         }
         
         sort(seq.begin(),seq.end());
