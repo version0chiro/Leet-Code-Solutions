e158af4412bea93c70a963589e6f721d69ce3597
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-01-24 10:47:51 +0530

    Time: 5 ms (9.80%), Space: 6.2 MB (19.00%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        
+        cout<<(int)'a'<<" "<<(int)'A'<<" "<<(int)'z'<<" "<<(int)'Z';
+        
+        int lowerCaseLL = 97,lowerCaseUL = 122, upperCaseLL = 65, upperCaseUL = 90;
+        
+        
+        int upperCount = 0;
+        int lowerCount = 0;
+        
+        for(auto a:word){
+            
+            if( (int)a<=upperCaseUL && (int)a>=upperCaseLL ){
+                upperCount++;
+                if(lowerCount>0) return false;
+            }
+            else{
+                lowerCount++;
+            }
+            
+        }
+        
+        if(lowerCount==word.size() || upperCount==word.size() || (upperCount==1  && lowerCount==word.size()-1)) return true;
+        
+        
+        
+        
+        
+        return false;
+    }
+};
