f0ffab6459da7b88ac1513085e3592a146b709f0
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-02-10 20:42:02 +0530

    Added solution - LeetHub

---
+++ Reverse an Array - GFG/reverse-an-array.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	//code
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++){
+	        int temp;
+	        cin>>temp;
+	        arr[i]=temp;
+	    }
+	    
+	   // for(int i=0;i<n/2;i++){
+	   //     int temp = arr[i];
+	   //     arr[i]=arr[n-i-1];
+	   //     arr[n-i-1]=temp;
+	   // }
+	    
+	    for(int i=n-1;i>=0;i--){
+	        cout<<arr[i]<<" ";
+	    }
+	    
+	    cout<<"\n";
+	    
+	}
+	return 0;
+}
