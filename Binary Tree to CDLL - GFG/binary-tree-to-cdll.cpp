8a1a7cb8a806cfc1667286d4b1ece4bd45d6cd7e
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-02-11 15:14:53 +0530

    Added solution - LeetHub

--- Binary Tree to CDLL - GFG/binary-tree-to-cdll.cpp
+++ Binary Tree to CDLL - GFG/binary-tree-to-cdll.cpp
@@ -123,90 +123,40 @@ struct Node
 class Solution
 {
   public:
-  
-Node *concatenate(Node *leftList, Node *rightList)
-{
-    // If either of the list is empty
-    // then return the other list
-    if (leftList == NULL)
-        return rightList;
-    if (rightList == NULL)
-        return leftList;
-  
-    // Store the last Node of left List
-    Node *leftLast = leftList->left;
-  
-    // Store the last Node of right List
-    Node *rightLast = rightList->left;
-  
-    // Connect the last node of Left List
-    // with the first Node of the right List
-    leftLast->right = rightList;
-    rightList->left = leftLast;
-  
-    // Left of first node points to
-    // the last node in the list
-    leftList->left = rightLast;
-  
-    // Right of last node refers to the first
-    // node of the List
-    rightLast->right = leftList;
-  
-    return leftList;
-}
-  
-// Function converts a tree to a circular Linked List
-// and then returns the head of the Linked List
-Node *bTreeToCList(Node *root)
-{
-    if (root == NULL)
-        return NULL;
-  
-    // Recursively convert left and right subtrees
-    Node *left = bTreeToCList(root->left);
-    Node *right = bTreeToCList(root->right);
-  
-    // Make a circular linked list of single node
-    // (or root). To do so, make the right and
-    // left pointers of this node point to itself
-    root->left = root->right = root;
-  
-    // Step 1 (concatenate the left list with the list 
-    //         with single node, i.e., current node)
-    // Step 2 (concatenate the returned list with the
-    //         right List)
-    return concatenate(concatenate(left, root), right);
-}
+    //Function to convert binary tree into circular doubly linked list.
+    
+    Node *solve(Node *left,Node *right){
+        if(left==NULL) return right;
+        if(right==NULL) return left;
+        
+        auto leftLast = left->left;
+        
+        auto rightLast = right->left;
+        
+        leftLast->right = right;
+        right->left = leftLast;
+        
+        left->left=rightLast;
+        rightLast->right = left;
+        
+        return left;
+    }
+    
+    Node *bTreeToCList(Node *root)
+    {
+    //add code here.
+        if(root==NULL) return NULL;
+        
+        Node *left = bTreeToCList(root->left);
+        
+        Node *right = bTreeToCList(root->right);
+        
+        root->left=root->right=root;
+        
+        return solve(solve(left,root),right);
+    }
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
 // { Driver Code Starts.
 int main() {
     int t;
