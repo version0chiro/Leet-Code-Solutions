5ea3fc3248580cac90a9688a0194b6a0a183e3eb
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-02-11 22:53:06 +0530

    Added solution - LeetHub

--- Stock span problem - GFG/stock-span-problem.cpp
+++ Stock span problem - GFG/stock-span-problem.cpp
@@ -13,43 +13,32 @@ class Solution
     vector <int> calculateSpan(int price[], int n)
     {
        // Your code here
-      vector<int> ans(n,1);
-    //   for(int i=0;i<n;i++){
-    //       int t = 1;
-    //       for(int j=i-1;j>=0;j--){
-    //           if(price[j]<=price[i]) t++;
-    //           else break;
-    //       }
-    //       ans.push_back(t);
-    //   }
+       vector<int> ans;
        
+       stack<int> s;
        
-    //   return ans;
-        
-        stack<int> st;
-        
-        st.push(0);
-        
-        ans[0] = (1);
-        
-        for(int i=1;i<n;i++){
-            while(!st.empty() && price[st.top()]<=price[i]) st.pop();
-            
-           ans[i] = (
-                st.empty()? i+1: i-st.top()
-                );
-            
-            st.push(i);
-                
-            
-        }
-        
-        return ans;
-    
+       ans.push_back(1);
+       s.push(0);
+       
+       for(int i=1;i<n;i++){
+           
+           while(s.size() && price[s.top()]<=price[i]){
+               s.pop();
+           }
+           
+           if(s.empty()){
+               ans.push_back(i+1);
+           }else{
+               ans.push_back(i-s.top());
+           }
+           
+           s.push(i);
+       }
+       
+       return ans;
     }
 };
 
-// 100
 
 
 // { Driver Code Starts.
