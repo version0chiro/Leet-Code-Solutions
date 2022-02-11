1b3a088038d9382f647f28b37767936fb0d38e33
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-02-11 16:27:38 +0530

    Added solution - LeetHub

---
+++ Print Anagrams Together - GFG/print-anagrams-together.cpp
@@ -0,0 +1,65 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        //code here
+        unordered_map<string,vector<string>> m;
+        
+        vector<vector<string>> ans;
+        
+        for(auto a:string_list){
+            string og_temp = a;
+            sort(a.begin(),a.end());
+            m[a].push_back(og_temp);
+        }
+        
+        for(auto a:m){
+            vector<string> temp;
+            for(auto p:a.second){
+                temp.push_back(p);
+            }
+            ans.push_back(temp);
+        }
+        
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
