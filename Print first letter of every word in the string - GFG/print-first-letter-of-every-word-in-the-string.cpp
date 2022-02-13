c9f78ea582b34172de8d2254a4d2ed0ff55f60a3
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-02-13 17:07:18 +0530

    Added solution - LeetHub

---
+++ Print first letter of every word in the string - GFG/print-first-letter-of-every-word-in-the-string.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	string firstAlphabet(string S)
+	{
+	    // Your code goes here
+	    string ans = "";
+	    
+	    ans+=S[0];
+	    
+	    for(int i=0;i<S.size()-1;i++){
+	        if(S[i]==' '){
+	            ans=ans+S[i+1];
+	        }
+	    }
+	    
+	    return ans;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	string tc;
+   	getline(cin, tc);
+   	t = stoi(tc);
+   	while(t--)
+   	{
+   		string s;
+   		getline(cin, s);
+
+   	
+        Solution ob;
+   		cout << ob.firstAlphabet(s) << "\n";
+   	}
+
+    return 0;
+} 
+  // } Driver Code Ends
