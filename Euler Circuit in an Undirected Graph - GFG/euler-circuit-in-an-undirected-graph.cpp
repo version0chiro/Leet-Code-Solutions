8834a27d0bf45421c2f13611c38aab2c019ef35a
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-02-14 16:11:27 +0530

    Added solution - LeetHub

---
+++ Euler Circuit in an Undirected Graph - GFG/euler-circuit-in-an-undirected-graph.cpp
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+public:
+    
+    void DFS(int i,vector<int> adj[],vector<bool> &visited){
+        visited[i]=true;
+        
+        for(auto a:adj[i]){
+            if(!visited[a]){
+                DFS(a,adj,visited);
+            }
+        }
+    }
+
+	bool isEularCircuitExist(int V, vector<int>adj[]){
+	    // Code here
+	    vector<bool> visited(V,false);
+	    
+	    int i=0;
+	    
+	    for(i=0;i<V;i++){
+	        if(adj[i].size()>0) break;
+	    }
+	    
+	    if(i==V) return true;
+	    
+	    DFS(i,adj,visited);
+	    
+	    for(int i=0;i<V;i++){
+	        if(adj[i].size()>0 && visited[i]==false) return 0;
+	    }
+	    
+	    int odd  =0;
+	    
+	    for(int i=0;i<V;i++){
+	        if(adj[i].size() & 1) odd++;
+	    }
+	    
+	    if(odd>2) return 0;
+	    
+	    return odd==0;
+	    
+	    
+	}
+
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		bool ans = obj.isEularCircuitExist(V, adj);
+		if(ans)
+			cout << "1\n";
+		else cout << "0\n";
+	}
+	return 0;
+}  // } Driver Code Ends
