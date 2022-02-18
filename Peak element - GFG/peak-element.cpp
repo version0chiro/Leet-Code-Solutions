ee6e47c938237df80fb914406b0c25b095694f2b
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-02-18 14:14:59 +0530

    Added solution - LeetHub

--- Peak element - GFG/peak-element.cpp
+++ Peak element - GFG/peak-element.cpp
@@ -12,27 +12,32 @@ using namespace std;
 class Solution
 {
     public:
-    int peakElement(int arr[], int n){
-   int high=n-2,low=1;
-   if(arr[0]>=arr[1]){
+    int peakElement(int arr[], int n)
+    {
+       // Your code here
+       int l =1,r=n-2;
+       
+       if(arr[0]>=arr[1]) return 0;
+       
+       if(arr[n-1]>=arr[n-2]) return n-1;
+       
+       while(l<=r){
+           int mid = l + ((r - l) / 2);
+           
+           if(mid<n-1 && mid>=1 && arr[mid]>=arr[mid-1] && arr[mid]>=arr[mid+1]) return mid;
+           
+           
+           
+           else if(arr[mid]<arr[mid+1]) l=mid+1;
+           
+           else  r = mid-1;
+       }
+       
+       
        return 0;
-   }
-   else if(arr[n-1]>=arr[n-2]){
-       return n-1;
-   }
-   while(low<=high){
-       int mid=(low+high)/2;
-       if(arr[mid]>=arr[mid-1] && arr[mid]>=arr[mid+1]){
-           return mid;
-       }
-       else if(arr[mid+1]>arr[mid]){
-           low=mid+1;
-       }
-       else{
-           high=mid-1;
-       }
-   }
-}
+       
+       
+    }
 };
 
 // { Driver Code Starts.
