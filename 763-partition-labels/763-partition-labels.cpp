300ec212dda95d54795f888e42075e336b634e5b
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-02-19 18:17:04 +0530

    Time: 4 ms (75.24%), Space: 6.6 MB (63.77%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,49 +1,30 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<int,pair<int,int>> m;
+        unordered_map<char,int> m;
         
         for(int i=0;i<s.size();i++){
-            if(m.find(s[i])==m.end()){
-                m[s[i]]={i,i};
-            }else{
-                m[s[i]].second=i;
+            m[s[i]]=i;
+        }
+        
+        
+        vector<int> ans;
+        
+        int end=m[s[0]];
+        int size = 1;
+        
+        for(int i=1;i<s.size();i++){
+            // cout<<size<<" "<<end<<"\n";
+            if(i>end){
+                ans.push_back(size);
+                size=0;
             }
+            size++;
+            end=max(end,m[s[i]]);
         }
         
-        vector<pair<int,int>> labels;
+        ans.push_back(size);
         
-        for(auto a:m){
-            labels.push_back({a.second.first,a.second.second});
-            
-        }
-        
-        sort(labels.begin(),labels.end());
-        
-        
-        vector<pair<int,int>> ans;
-        
-        int start = labels[0].first,end=labels[0].second;
-        
-        for(int i=1;i<labels.size();i++){
-            if(labels[i].first>end){
-                ans.push_back({start,end});
-                start=labels[i].first;
-                end=labels[i].second;
-            }else{
-                end=max(end,labels[i].second);
-            }
-        }
-        
-        ans.push_back({start,end});
-        
-        vector<int> finalA;
-        
-        for(auto a:ans){
-            finalA.push_back(a.second-a.first+1);
-        }
-        
-        return finalA;
-        
+        return ans;
     }
 };
