d539d79fd7090c023607de14283232e5a48266f2
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-02-21 14:50:23 +0530

    Time: 36 ms (96.57%), Space: 33.1 MB (16.89%) - LeetHub

--- 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.cpp
+++ 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.cpp
@@ -12,15 +12,9 @@
 class Solution {
 public:
     TreeNode* sufficientSubset(TreeNode* root, int limit) {
-        if(root->left == root->right){
-            return root->val<limit?NULL:root;
-        }
-        if(root->left){
-            root->left = sufficientSubset(root->left,limit-root->val);
-        }
-        if(root->right){
-            root->right = sufficientSubset(root->right,limit-root->val);
-        }
+        if(root->left==root->right) return root->val<limit?NULL:root;
+        if(root->left) root->left = sufficientSubset(root->left,limit-root->val);
+        if(root->right) root->right = sufficientSubset(root->right,limit-root->val);
         return root->left==root->right?NULL:root;
     }
 };
