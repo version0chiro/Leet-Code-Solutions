36cd1485a9aef851163a71598b642a8f7943e6fc
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-02-22 20:05:22 +0530

    Added solution - LeetHub

--- Remove loop in Linked List - GFG/remove-loop-in-linked-list.cpp
+++ Remove loop in Linked List - GFG/remove-loop-in-linked-list.cpp
@@ -77,41 +77,7 @@ class Solution
 {
     public:
     
-    void removeLoop2(struct Node* loop_node, struct Node* head)
-{
-    struct Node* ptr1 = loop_node;
-    struct Node* ptr2 = loop_node;
- 
-    // Count the number of nodes in loop
-    unsigned int k = 1, i;
-    while (ptr1->next != ptr2) {
-        ptr1 = ptr1->next;
-        k++;
-    }
- 
-    // Fix one pointer to head
-    ptr1 = head;
- 
-    // And the other pointer to k nodes after head
-    ptr2 = head;
-    for (i = 0; i < k; i++)
-        ptr2 = ptr2->next;
- 
-    /*  Move both pointers at the same pace,
-      they will meet at loop starting node */
-    while (ptr2 != ptr1) {
-        ptr1 = ptr1->next;
-        ptr2 = ptr2->next;
-    }
- 
-    // Get pointer to the last node
-    while (ptr2->next != ptr1)
-        ptr2 = ptr2->next;
- 
-    /* Set the next node of the loop ending node
-      to fix the loop */
-    ptr2->next = NULL;
-}
+    
     //Function to remove a loop in the linked list.
     void removeLoop(Node* head)
     {
