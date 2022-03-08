1ec4e1539990ba0c3f07f5b4c1383bc26f668110
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-08 18:33:33 +0530

    Added solution - LeetHub

--- Form a palindrome - GFG/form-a-palindrome.cpp
+++ Form a palindrome - GFG/form-a-palindrome.cpp
@@ -1,34 +1,33 @@
 // { Driver Code Starts
 //Initial template for C++
 
-#include <bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h> 
+using namespace std; 
 
  // } Driver Code Ends
 //User function template for C++
 
-class Solution{
-  public:
-    int countMin(string str){
-    //complete the function here
-        string str2 = str;
-        reverse(str2.begin(),str2.end());
+class Solution{   
+public:
+    int findMinInsertions(string S){
+        // code here
         
-        int s = str.size();
+        int n = S.size();
         
-        int t[s+1][s+1];
+        int t[n+1][n+1];
         
-        for(int i=0;i<s+1;i++){
+        string S2 = S;
+        
+        reverse(S2.begin(),S2.end());
+        
+        for(int i=0;i<=n;i++){
             t[i][0]=0;
-        }
-        
-        for(int i=0;i<s+1;i++){
             t[0][i]=0;
         }
         
-        for(int i=1;i<s+1;i++){
-            for(int j=1;j<s+1;j++){
-                if(str[i-1]==str2[j-1]){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(S[i-1]==S2[j-1]){
                     t[i][j]=1+t[i-1][j-1];
                 }else{
                     t[i][j]=max(t[i-1][j],t[i][j-1]);
@@ -36,21 +35,23 @@ class Solution{
             }
         }
         
-        return s-t[s][s];
+        return n-t[n][n];
     }
 };
 
 // { Driver Code Starts.
+
+
+
 int main(){
     int t;
-    cin >> t;
+    cin>>t;
     while(t--){
-        string str;
-        cin >> str;
+        string S;
+        cin>>S;
         Solution ob;
-        cout << ob.countMin(str) << endl;
+        cout<<ob.findMinInsertions(S)<<endl;
     }
-return 0;
+    return 0;
 }
-
   // } Driver Code Ends
