f71eabe9c4b5a1c7a317a64b931377adb9beb19c
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-08 11:35:07 +0530

    Added solution - LeetHub

--- Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
+++ Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
@@ -94,27 +94,24 @@ struct Node
 class Solution {
   public:
     // Function to return the diameter of a Binary Tree.
+    
     int solve(Node* root,int &ans){
         if(!root) return 0;
         
         int l = solve(root->left,ans);
-        
         int r = solve(root->right,ans);
         
-        ans = max(ans,l+r+1);
+        ans=max(ans,l+r+1);
         
-        return 1+max(l,r);
-        
+        return max(l,r)+1;
     }
     int diameter(Node* root) {
         // Your code here
         int ans = INT_MIN;
-        
         solve(root,ans);
-        
         return ans;
         
-    };
+    }
 };
 
 // { Driver Code Starts.
