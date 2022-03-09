43fb8cfa423ea105ac2f1075c3221b47f6b1b24d
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-09 10:15:58 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -10,24 +10,25 @@ class Solution{
     // N : Size of the Array arr[]
     // Function to count inversions in the array.
     
-      long long int merge(long long arr[],long long temp[],long long left,long long mid,long long right){
+    long long int merge(long long arr[],long long temp[],long long left, long long mid, long long right){
         long long int i,j,k;
-        long long int inv_count=0;
+        long long int inv_c=0;
         i=left;
         j=mid;
         k=left;
         
-        while((i<=mid-1) && (j<=right)){
+        while(i<mid && j<=right){
             if(arr[i]<=arr[j]){
-                temp[k++]=arr[i++];
+                temp[k++] = arr[i++];
+                
             }else{
                 temp[k++]=arr[j++];
-                
-                inv_count+=(mid-i);
+                inv_c+=mid-i;
             }
         }
         
-        while(i<=mid-1){
+        
+        while(i<mid){
             temp[k++]=arr[i++];
         }
         
@@ -35,34 +36,35 @@ class Solution{
             temp[k++]=arr[j++];
         }
         
-        for(i=left;i<=right;i++) arr[i]=temp[i];
-        
-        return inv_count;
-    }
-    
-    long long int mergeSort(long long arr[],long long temp[],int left,int right){
-        long long int mid,inv_count=0;
-        if(left<right){
-            mid=(left+right)/2;
-            
-            inv_count+=mergeSort(arr,temp,left,mid);
-            inv_count+=mergeSort(arr,temp,mid+1,right);
-            
-            inv_count+=merge(arr,temp,left,mid+1,right);
+        for(i=left;i<=right;i++){
+            arr[i]=temp[i];
         }
         
-        return inv_count;
+        return inv_c;
         
     }
     
+    long long int mergeSort(long long arr[],long long temp[],long long left, long long right){
+        long long int inv_c = 0;
+        
+        if(left<right){
+            long long int mid = (left+right)/2;
+            
+            inv_c+=mergeSort(arr,temp,left,mid);
+            inv_c+=mergeSort(arr,temp,mid+1,right);
+            
+            inv_c+=merge(arr,temp,left,mid+1,right);
+        }
+        
+        return inv_c;
+    }
     
     long long int inversionCount(long long arr[], long long N)
     {
         // Your Code Here
+        long long temp[N];
         
-        long long  temp[N];
-         
-         return mergeSort(arr,temp,0,N-1);
+        return mergeSort(arr,temp,0,N-1);
     }
 
 };
