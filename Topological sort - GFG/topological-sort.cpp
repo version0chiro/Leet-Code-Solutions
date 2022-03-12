fd8b6859223de0818c01e09e526fc4bf448cf487
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-12 13:14:25 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -7,52 +7,40 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
+	void dfs(int i,vector<int>adj[],vector<int> &ans,vector<bool> &vis){
+	    vis[i]=true;
+	    
+	    for(auto a:adj[i]){
+	        if(!vis[a]){
+	            dfs(a,adj,ans,vis);
+	        }
+	    }
+	    
+	    ans.push_back(i);
+	    
+	}
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
+	    
+	    vector<bool> vis(V+1,false);
+	    
 	    vector<int> ans;
 	    
-	    vector<bool> visited(V,false);
-	    
-	    queue<int> q;
-	    
-	    vector<int> indegree(V,0);
-	    
 	    for(int i=0;i<V;i++){
-	        for(auto a:adj[i]){
-	            indegree[a]++;
-	        }
+	        if(!vis[i]) dfs(i,adj,ans,vis);
 	    }
 	    
-	   // for(auto a:adj){
-	   //     for(int i=0;i<a.size();i++){
-	   //         indegree[a[i]]++;
-	   //     }
+	    
+	    reverse(ans.begin(),ans.end());
+	    
+	   // cout<<endl;
+	   // for(auto a:ans){
+	   //     cout<<a<<" ";
 	   // }
-	    
-	    for(int i=0;i<V;i++){
-	        if(indegree[i]==0){
-	            q.push(i);
-	            ans.push_back(i);
-	        }
-	    }
-	    
-	    while(q.size()){
-	        auto curr = q.front();
-	        q.pop();
-	        for(auto a:adj[curr]){
-	            if(indegree[a]>0){
-	                indegree[a]--;
-	                if(indegree[a]==0){
-	                    q.push(a);
-	                    ans.push_back(a);
-	                }
-	            }
-	        }
-	    }
+	   // cout<<endl;
 	    
 	    return ans;
-	    
 	}
 };
 
