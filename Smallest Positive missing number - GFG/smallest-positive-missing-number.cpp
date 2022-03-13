13b4219f36100e8abd5a47b0506308fb75f1c83b
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-13 10:59:51 +0530

    Added solution - LeetHub

--- Smallest Positive missing number - GFG/smallest-positive-missing-number.cpp
+++ Smallest Positive missing number - GFG/smallest-positive-missing-number.cpp
@@ -13,13 +13,17 @@ class Solution
     int missingNumber(int arr[], int n) 
     { 
         // Your code here
-        int oneFlag = false;
+        
+        // check if 1 ex
+        bool oneFlag = false;
+        
         for(int i=0;i<n;i++){
-            if(arr[i]==1) oneFlag=true;
+            if(arr[i]==1) oneFlag = true;
             if(arr[i]<1 || arr[i]>n) arr[i]=1;
         }
         
         if(!oneFlag) return 1;
+        
         
         for(int i=0;i<n;i++){
             int id = abs(arr[i]);
@@ -30,8 +34,9 @@ class Solution
             if(arr[i]>0) return i+1;
         }
         
-        
         return n+1;
+        
+        
     } 
 };
 
