7903de7ead05acea810fe9ef6ce59430abd72321
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-15 00:23:26 +0530

    Added solution - LeetHub

--- Knight Walk - GFG/knight-walk.cpp
+++ Knight Walk - GFG/knight-walk.cpp
@@ -9,46 +9,51 @@ using namespace std;
 
 class Solution {
 public:
-int minStepToReachTarget(vector<int>&Kp, vector<int>&Tp, int N){
-    vector<vector<int>> visited(N+1,vector<int>(N+1,false));
-    queue<vector<int>> q;
-    q.push({Kp[0],Kp[1],0});
-    int count = 0;
-    //Apply BFS over it's neightbours
+	int minStepToReachTarget(vector<int>&KnightPos, vector<int>&TargetPos, int N){
+	    // Code here
+	    queue<pair<pair<int,int>,int>> q;                                 
+	    q.push({{KnightPos[0],KnightPos[1]},0});
+	    
+	    int steps = 0;
+	    
+	    bool t[N+1][N+1];
+	    
+	    memset(t,false,sizeof(t));
+	    
+	    
     int dr[8][2] = {{-1,-2},{1,-2},{-2,-1},{-2,1},{-1,2},{1,2},{2,-1},{2,1}};
-    while(!q.empty())
-    {
-        int qSize = q.size();
-        for(int i=0;i<qSize;i++)
-        {
-            auto it = q.front();
-            int curi = it[0];
-            int curj = it[1];
-            q.pop();
-            if(curi == Tp[0] && curj == Tp[1])
-            {
-                return it[2];
-            }
-           // if(visited[curi][curj]){
-           //     continue;
-           // }
-            visited[curi][curj] = true;
-            for(int k=0;k<8;k++)
-            {
-                int dx = dr[k][0];
-                int dy = dr[k][1];
-                int nbri = dx+curi;
-                int nbrj = dy+curj;
-                if(nbri>0 && nbri<=N && nbrj>0 && nbrj<=N && !visited[nbri][nbrj])
-                {
-                    q.push({nbri,nbrj,it[2]+1});
-                    visited[nbri][nbrj] = true;
-                }
-            }
-        }
-    }
-    return 0;
-}
+	    
+	    while(q.size()){
+	        int s = q.size();
+	        for(int i=0;i<s;i++){
+	            auto top = q.front().first;
+	            auto dis = q.front().second;
+	            
+	            q.pop();
+	            
+	            t[top.first][top.second]=true;    
+	            
+	            if(top.first==TargetPos[0] && top.second==TargetPos[1]){
+	                return dis;
+	            }
+	            
+	            for(int k=0;k<8;k++){
+	                int dx = dr[k][0];
+	                int dy = dr[k][1];
+	                int nbri = dx+top.first;
+	                int nbrj = dy+top.second;
+	                
+	                if(nbri>0 && nbri<=N && nbrj>0 && nbrj<=N && !t[nbri][nbrj]){
+	                    q.push({{nbri,nbrj},dis+1});
+	                    t[nbri][nbrj]=true;
+	                }
+	            }
+	        }
+	           // steps++;
+	    }
+	    
+	    return 0;
+	}
 };
 
 // { Driver Code Starts.
