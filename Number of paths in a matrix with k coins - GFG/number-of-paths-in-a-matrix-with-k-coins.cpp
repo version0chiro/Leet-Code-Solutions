45324442d53cccabb0a0206169346b3620672250
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-15 01:39:24 +0530

    Added solution - LeetHub

---
+++ Number of paths in a matrix with k coins - GFG/number-of-paths-in-a-matrix-with-k-coins.cpp
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution {
+public:
+    long long dp[101][101][101];
+   long long check(int i,int j,int k,int n,vector<vector<int>>&a)
+   {
+       if(i>=n||j>=n)
+       return 0;
+       if(i==n-1&&j==n-1)
+       return k==a[i][j];
+       if(dp[i][j][k]!=-1)
+       return dp[i][j][k];
+       return dp[i][j][k]=check(i+1,j,k-a[i][j],n,a)+check(i,j+1,k-a[i][j],n,a);
+   }
+   long long numberOfPath(int n, int k, vector<vector<int>> a){
+       dp[n][n][k];
+       memset(dp,-1,sizeof(dp));
+     return check(0,0,k,n,a);
+   }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    Solution obj;
+    int i,j,k,l,m,n,t;
+    cin>>t;
+    while(t--)
+    {
+        cin>>k>>n;
+        vector<vector<int>> v(n+1, vector<int>(n+1, 0));
+        for(i=0;i<n;i++)
+            for(j=0;j<n;j++)
+                cin>>v[i][j];
+        cout << obj.numberOfPath(n, k, v) << endl;
+    }
+}  // } Driver Code Ends
