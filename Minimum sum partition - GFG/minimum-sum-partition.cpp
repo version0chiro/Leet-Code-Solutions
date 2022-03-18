29d1f3218cfd43030e82ce4ca9bcf3d7fc3cd023
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-18 19:26:23 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -6,65 +6,59 @@ using namespace std;
 class Solution{
 
   public:
-  
-  
 	int minDifference(int arr[], int n)  { 
 	    // Your code goes here
-	    
-	    int totalSum =0;
+	    int totalSum=0;
 	    
 	    for(int i=0;i<n;i++){
 	        totalSum+=arr[i];
-	    }
-	    int tSum=totalSum;
-	    totalSum=totalSum/2;
-	    
-	    bool t[n+1][totalSum+1];
-	    
-	    memset(t,0,sizeof(t));
-	    
-	    for(int i=0;i<n+1;i++){
-	        t[i][0]=1;
+	        
 	        
 	    }
 	    
-	    for(int i=0;i<totalSum+1;i++){
+	    int sum = totalSum/2;
+	    
+	    int t[n+1][sum+1];
+	    
+	    for(int i=0;i<=n;i++){
+	        t[i][0]=1;
+	    }
+	    
+	    for(int i=0;i<=sum;i++){
 	        t[0][i]=0;
 	    }
 	    
 	    t[0][0]=1;
 	    
-	    for(int i=1;i<n+1;i++){
-	        for(int j=1;j<totalSum+1;j++){
-	            
+	    for(int i=1;i<=n;i++){
+	        for(int j=1;j<=sum;j++){
 	            if(arr[i-1]<=j){
 	                t[i][j]=t[i-1][j-arr[i-1]] || t[i-1][j];
 	            }else{
 	                t[i][j]=t[i-1][j];
 	            }
-	            
 	        }
 	    }
 	    
-	    int k;
-	    
-	    int s1=0;
-	    
-	    for(k=totalSum;k>=0;k--){
-	        if(t[n][k]){
-	           s1=k;
+	   // for(int i=0;i<=n;i++){
+	   //     for(int j=0;j<=sum;j++){
+	   //         cout<<t[i][j]<<" ";
+	   //     }
+	   //     cout<<"\n";
+	   // }
+	    int s1;
+	    for(int i=sum;i>=0;i--){
+	        if(t[n][i]){
+	           s1= i;
 	           break;
 	        }
 	    }
 	    
-	    
-	    int s2 = tSum-s1;
-	    
-	    
-	    
+	    int s2 = totalSum-s1;
 	    
 	    return abs(s1-s2);
 	    
+	    return -1;
 	} 
 };
 
