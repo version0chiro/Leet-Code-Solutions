27e28e44d3bcb080e16250a127e42bc13997f7b2
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-19 10:06:12 +0530

    Added solution - LeetHub

--- Max sum in the configuration - GFG/max-sum-in-the-configuration.cpp
+++ Max sum in the configuration - GFG/max-sum-in-the-configuration.cpp
@@ -29,25 +29,5 @@ int main()
 /*You are required to complete this method*/
 int max_sum(int A[],int N)
 {
-    int cur_sum = 0;
-    int cur_val = 0;
-    for(int i=0;i<N;i++){
-        cur_sum+=A[i];
-        cur_val+=i*A[i];
-    }
-    
-    int res = cur_val;
-    
-    for(int i=1;i<N;i++){
-        int next_val = cur_val - (cur_sum - A[i-1]) + A[i-1]*(N-1);
-        
-        cur_val = next_val;
-        
-        res = max(cur_val,res);
-    }
-    
-    return res;
-    
-    
-    
+//Your code here
 }
