ee1a839f5fd78c9a9ea5e0129e9ce5b145ddf22a
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-20 10:56:27 +0530

    Added solution - LeetHub

--- Get minimum element from stack - GFG/get-minimum-element-from-stack.cpp
+++ Get minimum element from stack - GFG/get-minimum-element-from-stack.cpp
@@ -19,67 +19,44 @@ public :
 class Solution{
     int minEle;
     stack<int> s;
+    stack<int> minS;
     public:
     
        /*returns min element from stack*/
        int getMin(){
+           if(minS.size()==0) return -1;
            
+           return minS.top();
            //Write your code here
-           if(s.size()==0) return -1;
-           
-           return minEle;
        }
        
        /*returns poped element from stack*/
-       int pop()
-    {
-        if (s.empty())
-        {
-            // cout << "Stack is empty\n";
-            return -1;
-        }
- 
-        // cout << "Top Most Element Removed: ";
-        int t = s.top();
-        s.pop();
- 
-        // Minimum will change as the minimum element
-        // of the stack is being removed.
-        if (t < minEle)
-        {
-            // cout << minEle << "\n";
-            minEle = 2*minEle - t;
-            return (t+minEle)/2;
-        }
- 
-        else
-            return t;
-            // cout << t << "\n";
-    }
+       int pop(){
+           
+           if(s.size()==0) return -1;
+           
+           if(minS.size() && s.top()==minS.top()) {
+               minS.pop();
+           }
+           int ans = s.top();
+           s.pop();
+           
+           return ans;
+           //Write your code here
+       }
        
        /*push element x into the stack*/
-       void push(int x)
-    {
-        // Insert new number into the stack
-        if (s.empty())
-        {
-            minEle = x;
-            s.push(x);
-            
-            return;
-        }
- 
-        // If new number is less than minEle
-        else if (x < minEle)
-        {
-            s.push(2*x - minEle);
-            minEle = x;
-        }
- 
-        else
+       void push(int x){
            s.push(x);
- 
-    }
+           if(minS.size()==0){
+               minS.push(x);
+           }
+           
+           else if(minS.top()>=x){
+               minS.push(x);
+           }
+           //Write your code here
+       }
 };
 
 // { Driver Code Starts.
