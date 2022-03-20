f20b418e2898181bd689cd18311a89ba8c114266
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-20 11:01:51 +0530

    Added solution - LeetHub

---
+++ Intersection Point in Y Shapped Linked Lists - GFG/intersection-point-in-y-shapped-linked-lists.cpp
@@ -0,0 +1,113 @@
+// { Driver Code Starts
+#include<iostream>
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+int intersectPoint(struct Node* head1, struct Node* head2);
+
+Node* inputList(int size)
+{
+    if(size==0) return NULL;
+    
+    int val;
+    cin>> val;
+    
+    Node *head = new Node(val);
+    Node *tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n1,n2,n3;
+
+    cin>>T;
+    while(T--)
+    {
+        cin>>n1>>n2>>n3;
+        
+        Node* head1 = inputList(n1);
+        Node* head2 = inputList(n2);
+        Node* common = inputList(n3);
+        
+        Node* temp = head1;
+        while(temp!=NULL && temp->next != NULL)
+            temp = temp->next;
+        if(temp!=NULL) temp->next = common;
+        
+        temp = head2;
+        while(temp!=NULL && temp->next != NULL)
+            temp = temp->next;
+        if(temp!=NULL) temp->next = common;
+        
+        cout << intersectPoint(head1, head2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+//Function to find intersection point in Y shaped Linked Lists.
+int intersectPoint(Node* head1, Node* head2)
+{
+    // Your Code Here
+    Node* curr1 = head1;
+    Node* curr2 = head2;
+    
+    bool done1 = false;
+    
+    bool done2 = false;
+    
+    while(curr1!=curr2){
+        curr1=curr1->next;
+        curr2=curr2->next;
+        
+        if(curr1==NULL){
+            if(done1) return -1;
+            curr1=head2;
+            done1 = true;
+        }
+        
+        if(curr2==NULL){
+            if(done2) return -1;
+            curr2=head1;
+            done2=true;
+        }
+    }
+    
+    return curr1->data;
+}
+
