29041932cded4ac81ccbae152ad54394b05d2193
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-20 10:16:00 +0530

    Added solution - LeetHub

--- Remove loop in Linked List - GFG/remove-loop-in-linked-list.cpp
+++ Remove loop in Linked List - GFG/remove-loop-in-linked-list.cpp
@@ -76,45 +76,40 @@ struct Node
 class Solution
 {
     public:
-    
-    
     //Function to remove a loop in the linked list.
     void removeLoop(Node* head)
     {
         // code here
         // just remove the loop without losing any nodes
-        if(!head && !head->next) return;
-        Node* slow = head->next;
-        Node* fast= head->next->next;
+        if(head==NULL || !head->next) return;
         
-        while(fast && fast->next){
-            if(slow==fast) break;
-            
+        Node* slow = head->next;
+        Node* fast=head->next->next;
+        
+        while(fast && fast->next && fast!=slow){
             fast=fast->next->next;
             slow=slow->next;
         }
         
-        
-        
         if(fast==NULL || fast->next==NULL) return;
         
         
-         if(slow==fast ){
-            slow = head;
-            
+        if(slow==fast){
+            slow=head;
             if(slow==fast){
-                while(fast->next!=slow)fast=fast->next;
+                while(fast->next!=slow){
+                    fast=fast->next;
+                    
+                }
             }else{
                 while(fast->next!=slow->next){
-                    slow=slow->next;
                     fast=fast->next;
+                    slow=slow->next;
+                    
                 }
             }
-            fast->next=NULL;
+                fast->next=NULL;
         }
-        // tempSlow1->next = NULL;
-        
-        
     }
 };
 
