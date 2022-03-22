b1ea1263ea0367edac9d078edae95fbd044ec991
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-22 10:40:47 +0530

    Added solution - LeetHub

--- Maximum Path Sum between 2 Leaf Nodes - GFG/maximum-path-sum-between-2-leaf-nodes.cpp
+++ Maximum Path Sum between 2 Leaf Nodes - GFG/maximum-path-sum-between-2-leaf-nodes.cpp
@@ -92,36 +92,36 @@ struct Node
 
 class Solution {
 public:
-    int ans = INT_MIN;
-    int solve(Node* root){
-        if(!root) return 0;
-        
-        int l = solve(root->left);
-        int r= solve(root->right);
-        if(!root->left && !root->right) return root->data;
-        
-        if(!root->left) return root->data + r;
-        if(!root->right) return root->data +l;
-        
-        ans=max(ans,l+r+root->data);
-        
-        
-        return root->data+max(l,r);
-    }
-
+int ans = INT_MIN;
+int solve(Node* root){
+    if(!root) return 0;
+    
+    if(!root->left && !root->right) return root->data;
+    
+    int l = solve(root->left);
+    
+    int r = solve(root->right);
+    
+    if(!root->left) return root->data+r;
+    
+    if(!root->right) return root->data+l;
+    
+    ans=max(ans,root->data+l+r);
+    
+    return root->data+max(l,r);
+}
     int maxPathSum(Node* root)
     {
-        // code here
         if(!root) return 0;
         
         int res = solve(root);
         
-        
-        
-        if(!root->left || !root->right)
-        return max(res,ans);
+        if(!root->left || !root->right){
+            ans=max(res,ans);
+        }
         
         return ans;
+        // code here
     }
 };
 
