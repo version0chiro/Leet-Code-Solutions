721b60716040801f6d0871f61b0288c30c7a0517
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-24 16:28:11 +0530

    Added solution - LeetHub

--- Binary Tree to DLL - GFG/binary-tree-to-dll.cpp
+++ Binary Tree to DLL - GFG/binary-tree-to-dll.cpp
@@ -110,32 +110,30 @@ class Solution
     //Function to convert binary tree to doubly linked list and return it.
     
     void solve(Node* root,Node*& head){
-        
-        if(root==NULL) return;
+        if(!root) return;
         
         solve(root->right,head);
         
-        root->right = head;
+        root->right=head;
         
-        if(head!=NULL)
-            head->left = root;
+        if(head)
+            head->left=root;
             
-        head = root;
         
+        head=root;
         solve(root->left,head);
-        
         
     }
     
     Node * bToDLL(Node *root)
     {
-        // your code here
+        if(!root) return NULL;
         Node* head = NULL;
         
         solve(root,head);
         
         return head;
-        
+        // your code here
     }
 };
 
