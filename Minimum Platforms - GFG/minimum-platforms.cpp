4bc01068409b3d154c00ac23a322614c0eca50d3
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-03-24 16:24:07 +0530

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.cpp
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -13,33 +13,33 @@ class Solution{
     int findPlatform(int arr[], int dep[], int n)
     {
     	// Your code here
-    	
-    	// two pointers one for the arrival and one for dept, sort both the arrays and make one platform as the first train will need that
-    	// if arr[i]<=dep[j] means the next bus needs a new plat form so counter++, move to the next arrival (i++)
-        // if arr[i]>dep[j] means the next bus will come after the previous has left, so one platform less needed counter-- move to the next departure (j++)
-        
-        // at each point find max of counter
-    	int i =1,j=0;
+    	int i=1;
+    	int j=0;
     	
     	sort(arr,arr+n);
     	sort(dep,dep+n);
     	
+    // 	for(int i=0;i<n;i++){
+    // 	    cout<<arr[i]<<" "<<dep[i]<<" \n";
+    // 	}
+    	
+    // 	cout<<endl;
+    	
+    	int ans = 1;
     	int c = 1;
-    	int ans =1;
     	
     	while(i<n && j<n){
     	    if(arr[i]<=dep[j]){
     	        c++;
     	        i++;
-    	    }
-    	    
-    	    else if(arr[i]>dep[j]){
-    	        c--;
+    	    }else{
     	        j++;
+    	        c--;
     	    }
     	    
-    	    ans=max(c,ans);
+    	   // cout<<c<<" \n";
     	    
+    	    ans = max(ans,c);
     	}
     	
     	return ans;
