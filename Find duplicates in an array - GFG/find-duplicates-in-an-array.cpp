dd467daaf1cf6baaa6071ba2191a7a1c889401a2
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-04-01 16:59:59 +0530

    Added solution - LeetHub

--- Find duplicates in an array - GFG/find-duplicates-in-an-array.cpp
+++ Find duplicates in an array - GFG/find-duplicates-in-an-array.cpp
@@ -8,16 +8,15 @@ class Solution{
     vector<int> duplicates(int arr[], int n) {
         // code here
         for(int i=0;i<n;i++){
-            arr[arr[i]%n] = arr[arr[i]%n] + n;
+            arr[arr[i]%n]=arr[arr[i]%n]+n;
         }
         
         vector<int> ans;
         
         for(int i=0;i<n;i++){
-            if(arr[i]>=2*n){
-                ans.push_back(i);
-            }
+            if(arr[i]>=2*n) ans.push_back(i);
         }
+        
         if(ans.size()==0) return {-1};
         
         return ans;
