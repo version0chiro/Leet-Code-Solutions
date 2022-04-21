90adf4a6f8a1db7e332151b30463eb6f13f9e0b1
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-04-21 12:51:03 +0530

    Added solution - LeetHub

--- Peak element - GFG/peak-element.cpp
+++ Peak element - GFG/peak-element.cpp
@@ -15,28 +15,40 @@ class Solution
     int peakElement(int arr[], int n)
     {
        // Your code here
-       int l =1,r=n-2;
+       int l = 0;
+       int r= n-2;
        
-       if(arr[0]>=arr[1]) return 0;
-       
-       if(arr[n-1]>=arr[n-2]) return n-1;
-       
-       while(l<=r){
-           int mid = l + ((r - l) / 2);
-           
-           if(mid<n-1 && mid>=1 && arr[mid]>=arr[mid-1] && arr[mid]>=arr[mid+1]) return mid;
-           
-           
-           
-           else if(arr[mid]<arr[mid+1]) l=mid+1;
-           
-           else  r = mid-1;
+       if(n>=1 && arr[0]>arr[1]){
+           return 0;
        }
        
+       if(n>=1 && arr[n-1]>=arr[n-2])
+        return n-1;
        
-       return 0;
        
        
+       int ans;
+       
+       while(l<=r){
+            int mid = (l+r)/2;
+            
+           
+            
+           
+                if(arr[mid]>=arr[mid+1] && arr[mid]>=arr[mid-1]){
+                    // cout<<mid<<" ";
+                    return mid;
+                }
+                    
+                if(arr[mid+1]>=arr[mid])
+                    l=mid+1;
+                    
+                else
+                    r=mid-1;
+            
+            
+       }
+       return -1;
     }
 };
 
@@ -64,18 +76,17 @@ int main() {
 		    cout<<0<<endl;
 		else
 		{
-		if(n==1 and A==0)
-		f=1;
-		else
-		if(A==0 and a[0]>=a[1])
-		f=1;
-		else if(A==n-1 and a[n-1]>=a[n-2])
-		f=1;
-		else if(a[A]>=a[A+1] and a[A]>= a[A-1])
-		f=1;
-		else
-		f=0;
-		cout<<f<<endl;
+    		if(n==1 and A==0)
+    		    f=1;
+    		else if(A==0 and a[0]>=a[1])
+    		    f=1;
+    		else if(A==n-1 and a[n-1]>=a[n-2])
+    		    f=1;
+    		else if(a[A]>=a[A+1] and a[A]>= a[A-1])
+    		    f=1;
+    		else
+    		    f=0;
+    		cout<<f<<endl;
 		}
 		
 	}
