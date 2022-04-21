e53df60adde18a2fa42f1aa4ecb5a64aaa3fc3be
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-04-21 17:19:30 +0530

    Added solution - LeetHub

--- Smallest Positive missing number - GFG/smallest-positive-missing-number.cpp
+++ Smallest Positive missing number - GFG/smallest-positive-missing-number.cpp
@@ -13,18 +13,19 @@ class Solution
     int missingNumber(int arr[], int n) 
     { 
         // Your code here
-        
-        // check if 1 exists, then make all the numbers <1 and >n to 1, now make all the numbers their negative counter parts and loop through again if any number is positvie it shows that
-        // i was missing, if nothing found return n+1
-        bool oneFlag = false;
+        bool oneFound = false;
         
         for(int i=0;i<n;i++){
-            if(arr[i]==1) oneFlag = true;
-            if(arr[i]<1 || arr[i]>n) arr[i]=1;
+            if(arr[i]==1) oneFound=true;
+            
+            if(arr[i]>n || arr[i]<1) arr[i]=1;
         }
         
-        if(!oneFlag) return 1;
-        
+        if(oneFound==false){
+            // cout<<" no found";
+            return 1;
+            
+        }
         
         for(int i=0;i<n;i++){
             int id = abs(arr[i]);
@@ -36,8 +37,6 @@ class Solution
         }
         
         return n+1;
-        
-        
     } 
 };
 
