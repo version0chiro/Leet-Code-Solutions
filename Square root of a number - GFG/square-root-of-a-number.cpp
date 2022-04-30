ed2e2e398b788dc50b47a81be75289d2f6566a38
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-04-30 13:59:29 +0530

    Added solution - LeetHub

--- Square root of a number - GFG/square-root-of-a-number.cpp
+++ Square root of a number - GFG/square-root-of-a-number.cpp
@@ -14,22 +14,23 @@ class Solution{
         // Your code goes here   
         int l =1;
         int h = x;
-        
-        long long ans = 1;
+        long long int mid;
+        long long int ans;
         
         while(l<=h){
-            long mid = (l+h)/2;
+             mid = (l+h)/2;
             
-            long curRoot = mid*mid;
+            if(mid*mid==x){
+                return mid;
+            }
             
-            if(curRoot==x) return mid;
-            else if(curRoot>x) h=mid-1;
-            else{
+            if(mid*mid>x){
+                h=mid-1;
+            }else{
                 ans=mid;
                 l=mid+1;
             }
         }
-        
         
         return ans;
     }
