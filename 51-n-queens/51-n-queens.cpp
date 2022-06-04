ea20f289742ae58979fa699e4370a5aac903a312
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-06-04 20:07:36 +0530

    Time: 120 ms (5.79%), Space: 70.8 MB (5.44%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    bool isSafe(vector<string> &board,int _i,int _j){
-        
-        for(int i=0;i<board.size();i++){
-            if(board[i][_j]=='Q') return false;
-            
-            if(board[_i][i]=='Q') return false;
+    bool check(vector<string> &grid,int i,int j,int n){
+        for(int p=0;p<n;p++){
+            if(grid[i][p]=='Q' || grid[p][j]=='Q'){
+                return false;
+            }
         }
         
-        // cout<<" "<<_i<<" "<<_j<<" \n";
-        /*
-        
-        1,1
-        0,2
-        
-        */
-        
-        
-        for(int i=0;i<board.size();i++){
-            
-            if(_i+i<board.size() && _j+i<board.size() && board[_i+i][_j+i]=='Q') return false;
-            
-            if(i<=_i && i<=_j && board[_i-i][_j-i]=='Q') return false;
-            
-            if(i<=_i && _j+i<board.size() &&  board[_i-i][_j+i]=='Q') return false;
-            
-            if(i<=_j && i+_i<board.size() && board[_i+i][_j-i]=='Q') return false;
-            
+        int x=i,y=j;
+        while(x>=0 && y>=0){
+            if(grid[x][y]=='Q'){
+                return false;
+            }
+            x--;
+            y--;
         }
-        
+        x=i-1,y=j+1;
+        while(x>=0 && y<n){
+            if(grid[x][y]=='Q'){
+                return false;
+            }
+            x--;
+            y++;
+        }
         
         return true;
-        
     }
     void solve(int m,int n,vector<string> board,vector<vector<string>> &ans,int i,int j){
         if(n==0){
@@ -44,7 +37,7 @@ public:
         
         if(j>=m || i>=m) return;
         
-        if(isSafe(board,i,j)){
+        if(check(board,i,j,m)){
             board[i][j]='Q';
             // cout<<" placing queen at "<<i<<" "<<j<<" with "<<n<<" remaning "<<"\n";
             solve(m,n-1,board,ans,i+1,0);
