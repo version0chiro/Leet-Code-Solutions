8dcdb98c534d3f4df904e0b09df95db75c396336
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-06-15 14:51:13 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -11,20 +11,19 @@ class Solution{
     // Function to count inversions in the array.
     
     long long int merge(long long arr[],long long int l,long long int mid,long long int r){
-        long long int i,j,k;
+        long long int inv_c = 0;
+        
+        long long temp[r+1];
+        
+        long long int  i,j,k;
         
         i=l;
         j=mid;
         k=l;
         
-        long long int inv_c = 0;
-        
-        long long temp[r+1];
-        
         while(i<mid && j<=r){
             if(arr[i]<=arr[j]){
                 temp[k++]=arr[i++];
-                
             }else{
                 temp[k++]=arr[j++];
                 inv_c+=mid-i;
@@ -46,31 +45,24 @@ class Solution{
         return inv_c;
     }
     
-    long long int mergeS(long long arr[],long long int l,long long int r){
-        
-        long long inv_c = 0;
+    long long int mergeSort(long long arr[],int l,int r){
+        long long int inv_c=0;
         
         if(l<r){
-            
-            long long int mid = (l+r)/2;
-            
-            inv_c+=mergeS(arr,l,mid);
-            inv_c+=mergeS(arr,mid+1,r);
+            int mid = (l+r)/2;
+            inv_c+=mergeSort(arr,l,mid);
+            inv_c+=mergeSort(arr,mid+1,r);
             
             inv_c+=merge(arr,l,mid+1,r);
-            
             
         }
         
         return inv_c;
-        
     }
-    
     long long int inversionCount(long long arr[], long long N)
     {
         // Your Code Here
-        
-        return mergeS(arr,0,N-1);
+        return mergeSort(arr,0,N-1);
     }
 
 };
