5057015f6ee276b975dabbec4226649c7fe47506
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-06-18 23:37:44 +0530

    Added solution - LeetHub

--- Smallest window in a string containing all the characters of another string - GFG/smallest-window-in-a-string-containing-all-the-characters-of-another-string.cpp
+++ Smallest window in a string containing all the characters of another string - GFG/smallest-window-in-a-string-containing-all-the-characters-of-another-string.cpp
@@ -12,33 +12,29 @@ class Solution
     string smallestWindow (string s, string p)
     {
         // Your code here
-        int n1 = s.size();
-        int n2 = p.size();
+        int n1=s.size();
+        int n2=p.size();
         
-       int m1[256] = {0};
-        int m2[256] = {0};
+        int m1[256]={0};
+        int m2[256]={0};
         
         for(auto a:p){
             m2[a]++;
         }
         
-        int start = 0;
-        int start_index = -1;
+        int count =0;
+        int start =0 ;
+        int start_index=-1;
         int minLen=INT_MAX;
-        int i=0;
         
-        int c = 0;
-        
-        while(i<n1){
-            
+        for(int i=0;i<n1;i++){
             m1[s[i]]++;
             
-            if(m2[s[i]]>=m1[s[i]]){
-                // m2[p[i]]--;
-                c++;
+            if(m1[s[i]]<=m2[s[i]]){
+                count++;
             }
             
-            if(c==n2){
+            if(count==n2){
                 while(m1[s[start]]>m2[s[start]]){
                     if(m1[s[start]]>m2[s[start]]){
                         m1[s[start]]--;
@@ -46,22 +42,18 @@ class Solution
                     start++;
                 }
                 
-                int temp_length = i-start+1;
-                if(temp_length<minLen){
+                int tempLen = i-start+1;
+                
+                if(tempLen<minLen){
+                    minLen=tempLen;
                     start_index=start;
-                    minLen=temp_length;
                 }
             }
-            
-            i++;
         }
         
+        if(start_index==-1) return "-1";
         
-        if(start_index==-1){
-            return "-1";
-        }else{
-            return s.substr(start_index,minLen);
-        }
+        return s.substr(start_index,minLen);
         
     }
 };
