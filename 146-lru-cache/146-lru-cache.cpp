314c12e51b116b01fa296ef2c6d1a2d3e3218e0e
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-06-19 01:13:50 +0530

    Time: 755 ms (20.16%), Space: 181.3 MB (7.33%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,129 +1,115 @@
+
 class Node{
-        public:
-            int val;
-            int key;
-            Node* left;
-            Node* right;        
-    
-            Node(int data,int key){
-                this->val=data;
-                this->key=key;
-            }       
-            
+    public:
+        int key;
+        int data;
+        Node* next;
+        Node* prev;
+        
+        Node(int key,int data){
+            this->key = key;
+            this->data = data;
+        }
 };
 
+class LRUCache
+{
+    private:
 
-
-class LRUCache {
-public:
-    Node* head = new Node(-1,-1);
-    Node* tail = new Node(-1,-1);
+    public:
+    
     unordered_map<int,Node*> m;
-    int size = 0;
-    int cap;
+    int size;
+    int curr_size;
+    Node* head;
+    Node* tail;
     
-    LRUCache(int capacity) {
-        this->cap = capacity;
-        head->right = tail;
-        tail->left = head;
-            
+    //Constructor for initializing the cache capacity with the given value.
+    LRUCache(int cap)
+    {
+        size=cap;
+        curr_size=0;
+        head = new Node(-1,-1);
+        tail = new Node(-1,-1);
         
+        head->next = tail;
+        tail->prev=head;
+        // code here
     }
     
-    int get(int key) {
-        if(m.find(key)==m.end())
-            return -1;
-        
-        int val = m[key]->val;
-        
-        m[key]->left->right = m[key]->right;
-        m[key]->right->left=m[key]->left;
-        
-        m.erase(key);
-        
-        Node* temp = new Node(val,key);
-            
-        auto last_tail = tail->left;
-
-        // cout<<last_tail->val;
-
-        last_tail->right=temp;
-        tail->left = temp;
-
-        temp->right=tail;
-        temp->left=last_tail;
-
-        m[key] = temp;
-        
-        
-        return val;
-    }
-    
-    void put(int key, int value) {
+    //Function to return value corresponding to the key.
+    int get(int key)
+    {
         if(m.find(key)!=m.end()){
-             int val = m[key]->val;
+            int data = m[key]->data;
+            
+            Node* foundNode = m[key];
+            
+            foundNode->prev->next=foundNode->next;
+            foundNode->next->prev=foundNode->prev;
+            
+            m.erase(key);
+            
+            Node* newNode = new Node(key,data);
         
-        m[key]->left->right = m[key]->right;
-        m[key]->right->left=m[key]->left;
-        
-        m.erase(key);
-        
-        Node* temp = new Node(value,key);
-            
-        auto last_tail = tail->left;
+            Node* last_tail = tail->prev;
 
-        // cout<<last_tail->val;
+            last_tail->next=newNode;
+            newNode->prev = last_tail;
 
-        last_tail->right=temp;
-        tail->left = temp;
+            newNode->next=tail;
+            tail->prev=newNode;
+            // cout<<key<<" \n";
+            m[key]=newNode;
 
-        temp->right=tail;
-        temp->left=last_tail;
-
-        m[key] = temp;
-            return;
-        }
-        
-        if(size<cap){
-            Node* temp = new Node(value,key);
-            
-            auto last_tail = tail->left;
-            
-            // cout<<last_tail->val;
-            
-            last_tail->right=temp;
-            tail->left = temp;
-            
-            temp->right=tail;
-            temp->left=last_tail;
-         
-            m[key] = temp;
-            
-            size++;
+            return data;
         }else{
-            
-            auto next_head = head->right;
-            next_head->right->left = head;
-            head->right = next_head->right;
-            
-            m.erase(next_head->key);
-                
-            Node* temp = new Node(value,key);
-
-            auto last_tail = tail->left;
-
-            last_tail->right=temp;
-            tail->left = temp;
-            
-            temp->right=tail;
-            temp->left=last_tail;
-         
-            m[key] = temp;
-
+            return -1;
         }
+        // your code here
+    }
+    
+    //Function for storing key-value pair.
+    void put(int key, int value)
+    {
+        // your code here  
+        // cout<<curr_size<<" \n";
+        // cout<<key<<" "<<value<<" == ";
+        if(m.find(key)!=m.end()){
+            Node* foundNode = m[key];
+            
+            foundNode->prev->next=foundNode->next;
+            foundNode->next->prev=foundNode->prev;
+            
+            m.erase(key);
+        } else 
+            if(curr_size<size){
+            curr_size++;
+        }else{
+            Node* node_to_remove = head->next;
+            //  cout<<key<<" "<<value<<" == ";
+            // cout<<node_to_remove->key<<" \n";
+            node_to_remove->prev->next=node_to_remove->next;
+            node_to_remove->next->prev=node_to_remove->prev;
+            
+            m.erase(node_to_remove->key);
+        }
+        
+        Node* newNode = new Node(key,value);
+        
+        Node* last_tail = tail->prev;
+        
+        last_tail->next=newNode;
+        newNode->prev = last_tail;
+        
+        newNode->next=tail;
+        tail->prev=newNode;
+        // cout<<key<<" \n";
+        m[key]=newNode;
+        
+        
     }
 };
-
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache* obj = new LRUCache(capacity);
