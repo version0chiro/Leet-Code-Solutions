1f08e4370a033efbf21da360b3ef260d609a6f29
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-06-19 12:06:12 +0530

    Added solution - LeetHub

---
+++ Queue using two Stacks - GFG/queue-using-two-stacks.cpp
@@ -0,0 +1,83 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class StackQueue{
+private:
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int B);
+    int pop();
+
+};
+int main()
+{
+
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        StackQueue *sq = new StackQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+
+
+}
+// } Driver Code Ends
+
+
+/* The structure of the class is
+class StackQueue{
+private:   
+    // These are STL stacks ( http://goo.gl/LxlRZQ )
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int);
+    int pop();
+}; */
+
+//Function to push an element in queue by using 2 stacks.
+
+void StackQueue :: push(int x)
+{
+    // Your Code
+    s1.push(x);
+}
+
+//Function to pop an element from queue by using 2 stacks.
+int StackQueue :: pop()
+{
+    if(s1.empty() && s2.empty()) return -1;
+    if(s2.empty()){
+        while(s1.size()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+    
+    }
+    int ans =  s2.top();
+    
+    s2.pop();
+    
+    return ans;
+        // Your Code       
+}
