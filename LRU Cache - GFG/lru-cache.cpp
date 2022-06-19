823d90efe2e6d4289bfa50294daca4962e394e9e
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-06-19 10:27:46 +0530

    Added solution - LeetHub

--- LRU Cache - GFG/lru-cache.cpp
+++ LRU Cache - GFG/lru-cache.cpp
@@ -7,16 +7,16 @@ using namespace std;
 // design the class in the most optimal way
 
 class Node{
-    public:
-        int key;
-        int data;
-        Node* next;
-        Node* prev;
-        
-        Node(int key,int data){
-            this->key = key;
-            this->data = data;
-        }
+  public:
+    int key;
+    int value;
+    Node* next;
+    Node* prev;
+    
+    Node(int key,int data){
+        this->key=key;
+        this->value=data;
+    }
 };
 
 class LRUCache
@@ -24,43 +24,44 @@ class LRUCache
     private:
 
     public:
+    //Constructor for initializing the cache capacity with the given value.
     
     unordered_map<int,Node*> m;
-    int size;
     int curr_size;
+    int size;
     Node* head;
     Node* tail;
     
-    //Constructor for initializing the cache capacity with the given value.
+    
+    
     LRUCache(int cap)
     {
-        size=cap;
-        head = new Node(-1,-1);
-        tail = new Node(-1,-1);
         
-        head->next = tail;
+        size=cap;
+        curr_size=0;
+        head=new Node(-1,-1);
+        tail=new Node(-1,-1);
+        
+        head->next=tail;
         tail->prev=head;
         // code here
     }
     
+    void deleteNode(Node* nodeToBeDeleted){
+        nodeToBeDeleted->next->prev=nodeToBeDeleted->prev;
+        nodeToBeDeleted->prev->next=nodeToBeDeleted->next;
+    }
     //Function to return value corresponding to the key.
     int get(int key)
     {
-        if(m.find(key)!=m.end()){
-            int data = m[key]->data;
+        if(m.find(key)==m.end()) return -1;
+        
+        else{
+            int ans = m[key]->value;
             
-            // Node* foundNode = m[key];
+            set(key,ans);
             
-            // foundNode->prev->next=foundNode->next;
-            // foundNode->next->prev=foundNode->prev;
-            
-            // m.erase(key);
-            
-            set(key,data);
-            
-            return data;
-        }else{
-            return -1;
+            return ans;
         }
         // your code here
     }
@@ -68,39 +69,39 @@ class LRUCache
     //Function for storing key-value pair.
     void set(int key, int value)
     {
-        // your code here  
+        // your code here   
         if(m.find(key)!=m.end()){
-            Node* foundNode = m[key];
-            
-            foundNode->prev->next=foundNode->next;
-            foundNode->next->prev=foundNode->prev;
-            
+           
+            deleteNode(m[key]);
+           
             m.erase(key);
-        } else if(curr_size<size){
+            
+            
+            
+            
+        }else if(curr_size<size){
             curr_size++;
-            
-            
         }else{
-            Node* node_to_remove = head->next;
             
-            node_to_remove->prev->next=node_to_remove->next;
-            node_to_remove->next->prev=node_to_remove->prev;
+            auto nodeToBeDeleted = head->next;
             
-            m.erase(node_to_remove->key);
-        
-        
+            deleteNode(nodeToBeDeleted);
+            
+            m.erase(nodeToBeDeleted->key);
+            
         }
+        
         Node* newNode = new Node(key,value);
         
-        Node* last_tail = tail->prev;
-        
-        last_tail->next=newNode;
-        newNode->prev = last_tail;
+        tail->prev->next=newNode;
+        newNode->prev = tail->prev;
         
         newNode->next=tail;
         tail->prev=newNode;
         
+        
         m[key]=newNode;
+        
         
     }
 };
