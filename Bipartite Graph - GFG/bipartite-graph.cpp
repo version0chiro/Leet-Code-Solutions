e80cb132c27c314e25bf6d83ad4d8273d6ccf8ba
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2022-06-21 14:52:41 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -7,38 +7,34 @@ class Solution {
 
 public:
 
-    bool dfs(vector<int> adj[],int i,vector<int>& visited){
-        // visited[i]=c;
+    bool dfs(vector<int> adj[],int i,vector<int>& visited,int c){
+        visited[i]=c;
         
-        // int next_c = c==1?2:1;
+        int next_c = c==1?2:1;
         
         for(auto a:adj[i]){
-            if(visited[a]==-1){
-                visited[a]=!visited[i];
-                if(dfs(adj,a,visited)){
+            if(visited[a]==0){
+                if(dfs(adj,a,visited,next_c)){
                     return true;
                 }
-            }else if(visited[a]==visited[i]){
+            }else if(visited[a]==c){
                 return true;
             }
         }
         
         return false;
     }
-    
-    //  0->2
 
 	bool isBipartite(int V, vector<int>adj[]){
 	    // Code here
 	   //1 - >red 
 	   //2 -> green
 	   // 0 ->white
-	    vector<int> visited(V+1,-1);
+	    vector<int> visited(V,0);
 	    
 	    for(int i=0;i<V;i++){
-	        if(visited[i]==-1){
-	            visited[i]=0;
-    	        if(dfs(adj,i,visited)){
+	        if(visited[i]==0){
+    	        if(dfs(adj,i,visited,1)){
     	            return false;
     	        }
 	        }
